a906e8d10155c4abac26cd7981f9fb66a41490c8
xingyuwang1123 <287577487@qq.com>
2019-03-16 12:08:36 +0800

    Update client.c

--- client.c
+++ client.c
@@ -1,3 +1,22 @@
+/* -*- Mode: C; indent-tabs-mode: t; c-basic-offset: 4; tab-width: 4 -*-  */
+/*
+ * main.c
+ * Copyright (C) 2019 
+ * 
+ * tcp_client is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * tcp_client is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along
+ * with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include "unp.h"
 /*
 void str_cli(FILE* fp,int sockfd){
@@ -12,6 +31,75 @@ void str_cli(FILE* fp,int sockfd){
 	}
 }
 */
+/*
+void str_cli(FILE *fp,int sockfd){
+	int maxfdp1;
+	fd_set rset;
+	char sendline[MAXLINE],recvline[MAXLINE];
+
+	FD_ZERO(&rset);
+	for(;;){
+		FD_SET(fileno(fp),&rset);
+		FD_SET(sockfd,&rset);
+		maxfdp1=max(fileno(fp),sockfd)+1;
+		Select(maxfdp1,&rset,NULL,NULL,NULL);
+
+		if(FD_ISSET(sockfd,&rset))  //socket is readable
+		{
+			if(Readline(sockfd,recvline,MAXLINE)==0){
+				err_quit("str_cli:server terminated");
+			}
+			Fputs(recvline,stdout);
+		}
+		if(FD_ISSET(fileno(fp),&rset))  //standard input stream is readable
+		{
+			if(Fgets(sendline,MAXLINE,fp)==NULL){
+				return;   //all done
+			}
+			Writen(sockfd,sendline,strlen(sendline));
+		}
+	}
+}
+*/
+//version 3
+void str_cli(FILE *fp,int sockfd)
+{
+	int maxfdp1,stdineof;    //used to mark distant EOF
+	fd_set rset;
+	char buf[MAXLINE];      //change to buffer rather than line buf
+	int n;
+
+	stdineof=0;
+	FD_ZERO(&rset);
+	for(;;){
+		if(stdineof==0)
+			FD_SET(fileno(fp),&rset);
+		FD_SET(sockfd,&rset);
+		maxfdp1=max(fileno(fp),sockfd)+1;
+		Select(maxfdp1,&rset,NULL,NULL,NULL);
+
+		if(FD_ISSET(sockfd,&rset)){
+			if((n=Read(sockfd,buf,MAXLINE))==0){
+				if(stdineof==1)
+					return;  //normal termination
+				else
+					err_quit("str_cli:server terminated prematirely");
+			}
+			Write(fileno(fp),buf,n);	
+		}
+
+		if(FD_ISSET(fileno(fp),&rset)){
+			if((n=Read(fileno(fp),buf,MAXLINE))==0){
+				stdineof=1;
+				Shutdown(sockfd,SHUT_WR); //send FIN
+				FD_CLR(fileno(fp),&rset);
+				continue;
+			}
+			Writen(sockfd,buf,n);
+		}
+	}
+}
+
 
 int main(int argc,char** argv)
 {
